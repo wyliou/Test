f5eaada6e94568d3d2b6b5490ef1417bf8c322fb
Alex Liou <weiyuiliou@gmail.com>
2025-08-15 05:28:44 +0800

    first commit

---
+++ data_type.py
@@ -0,0 +1,33 @@
+first = "Alex"
+last = "Liou"
+
+# print(type(first))
+# print(type(first) == str)
+# print(isinstance(first, str))
+
+# construct function
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+# Concatenation
+# fullname = first + ' ' + last
+# print(fullname)
+
+# fullname += '!'
+# print(fullname)
+
+# # Casting a number to a string
+# decade = str(1980)
+# print(decade)
+# print(type(decade))
+
+# multiline = '''
+# Hey, How are you? 
+
+# I was just checking in.
+#                             All good?
+                            
+# '''
+# print(multiline)
